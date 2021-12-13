fb16a5ac00a0abff218456166a6f0f47a0672533
Hemaxi Narotamo <41121368+HemaxiN@users.noreply.github.com>
2021-12-13 07:12:49 +0000

    Add files via upload

--- fid_score_main.py
+++ fid_score_main.py
@@ -1,5 +1,5 @@
 from evaluation import *
 
 img1_dir = '/dev/shm/dataset3d/test/images' #test images
-img2_dir = '/dev/shm/dataset3d/syntheticimages' #synthetic images
+img2_dir = '/dev/shm/dataset3d/test/syntheticimages' #synthetic images
 compute_FID(img1_dir, img2_dir)
